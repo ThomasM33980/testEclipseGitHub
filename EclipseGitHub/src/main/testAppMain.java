1c205fae856dd36dea29644a29d994360d0a6d08
tom <thomas.millot91@gmail.com>
2021-01-05 12:39:34 +0100

    Update testing file

--- EclipseGitHub/src/main/testAppMain.java
+++ EclipseGitHub/src/main/testAppMain.java
@@ -1,4 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class testAppMain {
+	
+   String message = "Hello, EPSI 3";	
+   MessageUtil messageUtil = new MessageUtil(message);
 
+   @Test
+   public void testPrintMessage() {	  
+      assertEquals(message,messageUtil.printMessage());
+   }
 }
